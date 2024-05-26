51437a43c24ad83dcf829ba19418fd9ad25a94bb
Felix Gnass <fgnass@gmail.com>
2024-05-26 18:19:54 +0200

    use absolute position

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -265,7 +265,7 @@ export class Scene extends Component<Props> {
   render() {
     return (
       <div
-        class="fixed inset-0 max-h-[100svh] data-[throwing='0']:pointer-events-none"
+        class="absolute inset-0 max-h-[100svh] data-[throwing='0']:pointer-events-none"
         onClick={() => this.shake()}
         data-throwing={this.props.numberOfDice}
       />
