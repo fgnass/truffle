0141fb3d5d2319b7b0862da966877cf6359c120d
Felix Gnass <fgnass@gmail.com>
2024-05-26 14:41:57 +0200

    maskable icon

--- vite.config.ts
+++ vite.config.ts
@@ -13,17 +13,19 @@ export default defineConfig({
         short_name: "truffle",
         description: "A dice rolling game",
         display: "fullscreen",
-        theme_color: "#ffffff",
+        theme_color: "#6D28D9",
         icons: [
           {
             src: "/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any maskable",
           },
           {
             src: "/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any maskable",
           },
         ],
       },
